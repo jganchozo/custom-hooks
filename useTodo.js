bbed5b70aa5c532e45e0c3895e9e1c3ebb5cd9a8
Jose Manuel Ganchozo <jganchozo@outlook.com>
2023-03-13 07:36:51 -0400

    Initial commit

---
+++ useTodo.js
@@ -0,0 +1,69 @@
+import { useEffect, useReducer } from "react";
+import { todoReducer } from "../08-useReducer/todoReducer";
+
+
+const initialState = [
+    // {
+    //     id: new Date().getTime(),
+    //     description: 'Recolectar la piedra del alma',
+    //     done: false,
+    // }
+];
+
+const init = () => {
+    return JSON.parse(localStorage.getItem('todos')) || [];
+}
+
+export const useTodo = () => {
+
+    const [todos, dispatch] = useReducer(todoReducer, initialState, init);
+
+    useEffect(() => {
+        
+        localStorage.setItem('todos', JSON.stringify(todos) || []);
+
+    }, [todos]);
+    
+
+    const handleAdd = (todo) => {
+        console.log({ todo });
+
+        const action = {
+            type: '[TODO] add todo',
+            payload: todo,
+        }
+
+        dispatch(action);
+    }
+
+    const handleDelete = (id) => {
+        
+        const action = {
+            type: '[TODO] remove todo',
+            payload: id
+        }
+
+        dispatch(action);
+    }
+
+    const handleToggle = (id) => {
+
+        const action = {
+            type: '[TODO] toggle todo',
+            payload: id
+        }
+
+        dispatch(action);
+    } 
+
+
+    return {
+        todos,
+        ...todos,
+        handleAdd,
+        handleDelete,
+        handleToggle,
+        todosCount: todos.length,
+        pendingTodosCount: todos.filter(todo => !todo.done).length,
+    }
+}
